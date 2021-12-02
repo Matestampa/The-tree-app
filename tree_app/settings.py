700cfc2443cb461b044453ca778852ddbceb3c3d
Matew04 <paredpiso5@gmail.com>
2021-12-02 14:21:03 -0300

    configuration 1.1

--- tree_app/settings.py
+++ tree_app/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -79,12 +80,6 @@ WSGI_APPLICATION = 'tree_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-"""DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}"""
 
 import dj_database_url
 from decouple import config
@@ -129,6 +124,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_ROOT=os.path.join(BASE_DIR,"staticfiles")
 
 STATIC_URL = '/static/'
 
